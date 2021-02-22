924ec8e5447e97ce6ee78a52e34fbac2b8329608
Oscar <osandahl@deloittedigital.se>
2021-02-22 15:26:50 +0100

    Connect searchbar and background Outlets to Viewcontroller

--- unsplash-ios/unsplash-ios/ViewController.swift
+++ unsplash-ios/unsplash-ios/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
