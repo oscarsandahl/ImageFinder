ca7c9a014e15f9c2e886f371e405778c06fd2393
Oscar <osandahl@deloittedigital.se>
2021-02-26 08:46:25 +0100

    Added storyboard and viewcontroller to photoGrid

---
+++ unsplash-ios/unsplash-ios/Scene/QueryGrid/QueryGridViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QueryGridViewController.swift
+//  unsplash-ios
+//
+//  Created by Sandahl, Oscar on 2021-02-26.
+//
+
+import UIKit
+
+class QueryGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
