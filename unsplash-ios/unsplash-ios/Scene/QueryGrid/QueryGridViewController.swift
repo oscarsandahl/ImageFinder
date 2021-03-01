3d08b2f34c3857e7ba66bb032b2c5dfd704d3164
Oscar <osandahl@deloittedigital.se>
2021-03-01 15:13:12 +0100

    Revert "Added presenter and moved fetchQuery function from viewcontroller"
    This reverts commit 6a16a32deb0ee6dc467476348f3ea03af48cfd16.

--- unsplash-ios/unsplash-ios/Scene/QueryGrid/QueryGridViewController.swift
+++ unsplash-ios/unsplash-ios/Scene/QueryGrid/QueryGridViewController.swift
@@ -8,31 +8,22 @@
 import UIKit
 
 class QueryGridViewController: UIViewController {
-    
-    // MARK: - Variables
+
     var collectionView: UICollectionView!
-    var presenter: QueryGirdPresenter!
+    var searchUrl: String?
+    var queryResult: QueryResult?
     
-    // MARK: - Initialization
-    class func initViewController(query: String) -> UIViewController {
-        let queryGridViewController = QueryGridViewController()
-        queryGridViewController.presenter = QueryGirdPresenter(queryGridViewController, query: query)
-        return queryGridViewController
-    }
-    
-    // MARK: - Managing view
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.isNavigationBarHidden = false
         setupCollectionView()
-        presenter.fetchQuery()
+        fetchQuery()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
     }
     
-    // MARK: - Functions
     func setupCollectionView() {
         collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: UICollectionViewFlowLayout())
         collectionView.backgroundColor = .white
@@ -41,16 +32,32 @@ class QueryGridViewController: UIViewController {
         collectionView.register(QueryGridCell.nib, forCellWithReuseIdentifier: QueryGridCell.reuseIdentifier)
         self.view.addSubview(collectionView)
     }
+    
+    func fetchQuery() {
+        if let searchUrl = searchUrl {
+            APIManager.shared.getPhotosFromQuery(query: searchUrl, itemsPerPage: 24) { (result) in
+                switch result {
+                case.success(let data):
+                    DispatchQueue.main.async {
+                        self.queryResult = data
+                        self.collectionView.reloadData()
+                    }
+                case.failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+
 }
 
-// MARK: - CollectionView data source
 extension QueryGridViewController: UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if let queryResult = presenter.queryResult?.results {
+        if let queryResult = queryResult?.results {
             return queryResult.count
         } else {
             return 0
@@ -61,7 +68,7 @@ extension QueryGridViewController: UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: QueryGridCell.reuseIdentifier, for: indexPath)
         if let cell = cell as? QueryGridCell {
             cell.showSpinner(isLoading: true)
-            if let queryResult = presenter.queryResult?.results {
+            if let queryResult = queryResult?.results {
                 if let imageUrls = queryResult[indexPath.row].urls {
                     if let image = imageUrls["full"] {
                         cell.image.fetchImageFromURL(from: image) { (result) in
@@ -82,12 +89,10 @@ extension QueryGridViewController: UICollectionViewDataSource {
     }
 }
 
-// MARK: - CollectionView delegate
 extension QueryGridViewController: UICollectionViewDelegate {
     
 }
 
-// MARK: - CollectionView flow layout
 extension QueryGridViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView,
                         layout collectionViewLayout: UICollectionViewLayout,
