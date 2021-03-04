d1db5adc1d37bb432e209dff3b1c61c7890249ac
Oscar <osandahl@deloittedigital.se>
2021-03-04 10:57:42 +0100

    Add marks setup

--- unsplash-ios/unsplash-ios/Scene/ExpandedPhoto/ExpandedPhotoViewController.swift
+++ unsplash-ios/unsplash-ios/Scene/ExpandedPhoto/ExpandedPhotoViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class ExpandedPhotoViewController: UIViewController {
 
+    // MARK: - Variables
     @IBOutlet weak var expandedPhoto: UIImageView!
     
+    // MARK: - Initialization
+    
+    // MARK: - Managing view
     override func viewDidLoad() {
         super.viewDidLoad()
 
