8ef091a3b04a869a74986474eb54e5e9506462a7
Oscar <osandahl@deloittedigital.se>
2021-03-04 10:42:19 +0100

    Add expandedphoto vc + presenter and connect storyboard

---
+++ unsplash-ios/unsplash-ios/Scene/ExpandedPhoto/ExpandedPhotoPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  ExpandedPhotoPresenter.swift
+//  unsplash-ios
+//
+//  Created by Sandahl, Oscar on 2021-03-04.
+//
+
+import Foundation
+
+class ExpandedPhotoPresenter {
+    
+}
---
+++ unsplash-ios/unsplash-ios/Scene/ExpandedPhoto/ExpandedPhotoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExpandedPhotoViewController.swift
+//  unsplash-ios
+//
+//  Created by Sandahl, Oscar on 2021-03-04.
+//
+
+import UIKit
+
+class ExpandedPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+}
