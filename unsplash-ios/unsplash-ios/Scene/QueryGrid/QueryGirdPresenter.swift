8ba5dc0ddd90708a3ed8d010b935f94faad72f22
Oscar <osandahl@deloittedigital.se>
2021-03-10 08:47:37 +0100

    Add MARK's to QueryGridPresenter

--- unsplash-ios/unsplash-ios/Scene/QueryGrid/QueryGirdPresenter.swift
+++ unsplash-ios/unsplash-ios/Scene/QueryGrid/QueryGirdPresenter.swift
@@ -8,15 +8,19 @@
 import Foundation
 
 class QueryGirdPresenter {
+    
+    // MARK: - Variables
     weak var view: QueryGridViewController?
     var queryResult: QueryResult?
     var query: String?
     
+    // MARK: - Initialization
     init(_ view: QueryGridViewController, query: String) {
         self.view = view
         self.query = query
     }
     
+    // MARK: - Functions
     func fetchQuery() {
         guard let query = query else { return }
         APIManager.shared.getPhotosFromQuery(query: query, itemsPerPage: 24) { (result) in
