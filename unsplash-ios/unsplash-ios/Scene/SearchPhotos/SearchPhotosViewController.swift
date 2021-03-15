e9e6633f5570aeaa36b4813be39b217448054f78
Oscar <osandahl@deloittedigital.se>
2021-03-15 11:16:40 +0100

    Remove trailing whitespace in SearchPhotosViewController

--- unsplash-ios/unsplash-ios/Scene/SearchPhotos/SearchPhotosViewController.swift
+++ unsplash-ios/unsplash-ios/Scene/SearchPhotos/SearchPhotosViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Kingfisher
 
 class SearchPhotosViewController: UIViewController {
-    
+
     // MARK: - Constants
     private static let storyboardName = "Main"
     private static let viewControllerIdentifier = "SearchPhotosId"
@@ -18,30 +18,30 @@ class SearchPhotosViewController: UIViewController {
     @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
-    
+
     // MARK: - Variables
     var presenter: SearchPhotosPresenter!
-    
+
     // MARK: - Initialization
     class func initViewController() -> UIViewController {
         let storyboard = UIStoryboard(name: SearchPhotosViewController.storyboardName, bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: SearchPhotosViewController.viewControllerIdentifier)
-        
+
         if let searchPhotosViewController = viewController as? SearchPhotosViewController {
             searchPhotosViewController.presenter = SearchPhotosPresenter(searchPhotosViewController)
-            
+
         }
-        
+
         return viewController
     }
-    
+
     // MARK: - Managing view
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
         presenter.fetchBackground()
     }
-    
+
     // MARK: - Functions
     func showSpinner(isLoading: Bool) {
         if isLoading {
@@ -52,7 +52,7 @@ class SearchPhotosViewController: UIViewController {
             self.loadingIndicator.stopAnimating()
         }
     }
-    
+
 }
 
 // MARK: - Searchbar delegate
