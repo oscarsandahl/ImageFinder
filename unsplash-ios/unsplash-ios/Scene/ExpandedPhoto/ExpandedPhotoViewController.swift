c6f864015e41783af1a956784bd71646d6eb36f7
Oscar <osandahl@deloittedigital.se>
2021-03-15 11:15:35 +0100

    Remove trailing whitespace in EpandedPhotoViewController

--- unsplash-ios/unsplash-ios/Scene/ExpandedPhoto/ExpandedPhotoViewController.swift
+++ unsplash-ios/unsplash-ios/Scene/ExpandedPhoto/ExpandedPhotoViewController.swift
@@ -8,37 +8,37 @@
 import UIKit
 
 class ExpandedPhotoViewController: UIViewController {
-    
+
     // MARK: - Constants
     private static let storyboardName = "Main"
     private static let viewControllerIdentifier = "ExpandedPhotoId"
-    
+
     // MARK: - Outlets
     @IBOutlet weak var expandedPhoto: UIImageView!
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
-    
+
     // MARK: - Variables
     var presenter: ExpandedPhotoPresenter!
-    
+
     // MARK: - Initialization
     class func initViewController(imageUrl: String) -> UIViewController {
         let storyboard = UIStoryboard(name: ExpandedPhotoViewController.storyboardName, bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: ExpandedPhotoViewController.viewControllerIdentifier)
-        
+
         if let expandedPhotoViewController = viewController as? ExpandedPhotoViewController {
             expandedPhotoViewController.presenter = ExpandedPhotoPresenter(expandedPhotoViewController, imageUrl: imageUrl)
-            
+
         }
-        
+
         return viewController
     }
-    
+
     // MARK: - Managing view
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.fetchImage()
     }
-    
+
     // MARK: - Functions
     func showSpinner(isLoading: Bool) {
         if isLoading {
@@ -49,5 +49,5 @@ class ExpandedPhotoViewController: UIViewController {
             self.loadingIndicator.stopAnimating()
         }
     }
-  
+
 }
