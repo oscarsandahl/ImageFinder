8c0fd7d7427eba60b6ebeb0552ec438d86590a5f
Oscar <osandahl@deloittedigital.se>
2021-06-08 16:45:02 +0200

    Add unit test init and basic unit tests

--- unsplash-ios/APIManagerTests/APIManagerTests.swift
+++ unsplash-ios/APIManagerTests/APIManagerTests.swift
@@ -6,15 +6,43 @@
 //
 
 import XCTest
+@testable import unsplash_ios
 
 class APIManagerTests: XCTestCase {
 
+    var sut: APIManager!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = APIManager()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
 
+    func testAccessKey() {
+        XCTAssertEqual(sut.accessKey, "EtWsatXcwQKXGTQgPNh1mQUO3HR_LoW0Z_ZpxU7y_PA")
+    }
+
+    func testSecretKey() {
+        XCTAssertEqual(sut.secretKey, "ra95FPU6vtIexK5w0R5F59a5ILGwOY2vVMQ8QYR3yjU")
+    }
+
+    func testBaseUrl() {
+        XCTAssertEqual(sut.baseUrl, "https://api.unsplash.com/")
+    }
+
+    func testRandomPhotoEndPoint() {
+        XCTAssertEqual(sut.randomPhotoEndpoint, "photos/random?")
+    }
+
+    func testSearchPhotoEndPoint() {
+        XCTAssertEqual(sut.searchPhotoEndpoint, "search/photos?")
+    }
+
+    func testGetAccessKey() {
+        XCTAssertEqual(sut.getAccessKey(), "client_id=EtWsatXcwQKXGTQgPNh1mQUO3HR_LoW0Z_ZpxU7y_PA")
+    }
 }
